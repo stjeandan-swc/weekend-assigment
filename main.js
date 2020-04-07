fa35fe749f94e2dd925e6c5507a006f732189a77
stjeandan-swc <stjean.dan.dev@gmail.com>
2020-04-07 17:30:43 -0400

    added temporary Contact prompt()

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
-
 let isSidenavOpen = false;
 
 function openSidenav() {
   if(!isSidenavOpen) {
-    document.getElementById("sidenav").classList.toggle("show-sidenav")
+    document.getElementById("sidenav").classList.toggle("show-sidenav");
     isSidenavOpen = true;
   }
 }
 
 function closeSidenav() {
   if(isSidenavOpen) {
-    document.getElementById("sidenav").classList.toggle("show-sidenav")
+    document.getElementById("sidenav").classList.toggle("show-sidenav");
     isSidenavOpen = false;
+  }
+}
+
+function contactBtn() {
+  let msg = prompt("Please send us a message.");
+
+  if(msg != null && msg != "") {
+      userEmail = prompt("Please leave us your Email");
+
+      if(userEmail != null && userEmail != "") {
+        alert("Thank you for your message. We will respond in the next 2 days.");
+      }
+      else {
+        alert("Sorry but we did not get your email and have no way to respond.");
+      }
+  }
+  else {
+    alert("Sorry but we did not get an acceptable message from you.");
   }
 }
