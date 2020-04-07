e4b6d8c9bf88acf659639b7bc01b31e213d01fc6
stjeandan-swc <stjean.dan.dev@gmail.com>
2020-04-07 16:44:01 -0400

    Added a sidenav for smaller screen sizes UI and functionality

---
+++ main.js
@@ -0,0 +1,16 @@
+
+let isSidenavOpen = false;
+
+function openSidenav() {
+  if(!isSidenavOpen) {
+    document.getElementById("sidenav").classList.toggle("show-sidenav")
+    isSidenavOpen = true;
+  }
+}
+
+function closeSidenav() {
+  if(isSidenavOpen) {
+    document.getElementById("sidenav").classList.toggle("show-sidenav")
+    isSidenavOpen = false;
+  }
+}
